37646be9ad147d9ce01de5192ba8e5c88ecba723
suinpark <suinpark@gmail.com>
2024-09-05 23:56:10 +0900

    Feat: BoardDao 테스트 위한 생성자 추가
    - insert 및 update 기능에 대한 BoardDao 테스트 위한 생성자 추가

--- happy_diary/src/main/java/com/happydiary/dto/BoardDto.java
+++ happy_diary/src/main/java/com/happydiary/dto/BoardDto.java
@@ -23,6 +23,27 @@ public class BoardDto {
     String up_id;
     String up_date;
 
+    // 게시물 작성
+    public BoardDto(String title, String content, String img_path, String writer, String praise_target, String is_public, String is_notice) {
+        this.title = title;
+        this.content = content;
+        this.img_path = img_path;
+        this.writer = writer;
+        this.praise_target = praise_target;
+        this.is_public = is_public;
+        this.is_notice = is_notice;
+    }
+
+    // 게시물 수정
+    public BoardDto(Integer bno, String title, String content, String img_path, String praise_target, String is_public) {
+        this.bno = bno;
+        this.title = title;
+        this.content = content;
+        this.img_path = img_path;
+        this.praise_target = praise_target;
+        this.is_public = is_public;
+    }
+
     @Override
     public String toString() {
         return "BoardDto{" +
