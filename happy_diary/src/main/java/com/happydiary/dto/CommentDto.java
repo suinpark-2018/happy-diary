71fd91aed5360af32562ffc0b57b67e4762a1a19
suinpark <suinpark@gmail.com>
2024-09-13 15:24:55 +0900

    Refactor: 댓글 기능 구현 위해 타입 수정 및 생성자 추가

--- happy_diary/src/main/java/com/happydiary/dto/CommentDto.java
+++ happy_diary/src/main/java/com/happydiary/dto/CommentDto.java
@@ -2,34 +2,41 @@ package com.happydiary.dto;
 
 import lombok.*;
 
+import java.util.List;
+
 @Data
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class CommentDto {
-    Integer cno;
-    Integer bno;
-    Integer parent_cno;
-    String writer;
-    String content;
+    int cno; // 댓글 번호
+    int bno; // 게시물 번호
+    Integer parent_cno; // 상위 댓글
+    String writer; // 작성자
+    String content; // 내용
+    String status; // 삭제여부
+
+    // 시스템 컬럼
     String reg_id;
     String reg_date;
     String up_id;
     String up_date;
 
-    @Override
-    public String toString() {
-        return "CommentDto{" +
-                "cno=" + cno +
-                ", bno=" + bno +
-                ", parent_cno=" + parent_cno +
-                ", writer='" + writer + '\'' +
-                ", content='" + content + '\'' +
-                ", reg_id='" + reg_id + '\'' +
-                ", reg_date='" + reg_date + '\'' +
-                ", up_id='" + up_id + '\'' +
-                ", up_date='" + up_date + '\'' +
-                '}';
+    private List<CommentDto> replies; // 답글 목록
+
+    public CommentDto(Integer bno, Integer parent_cno, String writer, String content) {
+        this.bno = bno;
+        this.parent_cno = parent_cno;
+        this.writer = writer;
+        this.content = content;
+    }
+
+    public CommentDto(Integer cno, Integer bno, Integer parent_cno, String writer, String content) {
+        this.cno = cno;
+        this.bno = bno;
+        this.parent_cno = parent_cno;
+        this.writer = writer;
+        this.content = content;
     }
 }
