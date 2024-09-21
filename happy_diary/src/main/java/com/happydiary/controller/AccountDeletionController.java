4b319b4bad3469a6169c39b4322380ecc7e48040
suinpark <suinpark@gmail.com>
2024-09-21 12:23:27 +0900

    Feat: 회원탈퇴 기능 추가 위해 AccountDeletionController 구현
    - 회원탈퇴 기능에 대한 AccountDeletionController 구현 완료

---
+++ happy_diary/src/main/java/com/happydiary/controller/AccountDeletionController.java
@@ -0,0 +1,30 @@
+package com.happydiary.controller;
+
+import com.happydiary.service.UserServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+@Controller
+@RequestMapping("/delete")
+@RequiredArgsConstructor
+public class AccountDeletionController {
+    private final UserServiceImpl userService;
+
+    @PostMapping("/account")
+    public String accountDeletion(HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        String id = (String) session.getAttribute("userId");
+
+        if (!id.isEmpty() || !id.isBlank()) {
+            userService.deleteUserAccount(id);
+            session.invalidate();
+        }
+
+        return "redirect:/";
+    }
+}
