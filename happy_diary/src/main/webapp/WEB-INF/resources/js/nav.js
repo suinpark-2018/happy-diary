39c5dd877f4bb095e29d891f31e3985173459be4
suinpark <suinpark@gmail.com>
2024-09-24 12:01:19 +0900

    Refactor: 일부 CSS, JS 코드 수정

--- happy_diary/src/main/webapp/WEB-INF/resources/js/nav.js
+++ happy_diary/src/main/webapp/WEB-INF/resources/js/nav.js
@@ -1,26 +1,26 @@
-// 사용자 로그아웃 버튼 이벤트 처리
-$('#logout').on('click', function(event) {
-    event.preventDefault(); // 기본 동작 막기
+    // 사용자 로그아웃 버튼 이벤트 처리
+    $('#logout').on('click', function(event) {
+        event.preventDefault(); // 기본 동작 막기
 
-    if (confirm('로그아웃 하시겠습니까?')) {
-        $.get('/login/out', function() {
-            alert('로그아웃 되었습니다.');
-            window.location.href = "/login/form"; // 로그인 페이지로 리디렉션
-        }).fail(function() {
-            alert('로그아웃에 실패했습니다.');
-        });
-    }
-});
+        if (confirm('로그아웃 하시겠습니까?')) {
+            $.get('/login/out', function() {
+                alert('로그아웃 되었습니다.');
+                window.location.href = "/login/form"; // 로그인 페이지로 리디렉션
+            }).fail(function() {
+                alert('로그아웃에 실패했습니다.');
+            });
+        }
+    });
 
-$('#accountDeletion').on('click', function(event) {
-    event.preventDefault(); // 기본 동작 막기
+    $('#accountDeletion').on('click', function(event) {
+        event.preventDefault(); // 기본 동작 막기
 
-    if (confirm('정말로 탈퇴 하시겠습니까?')) {
-        $.post('/delete/account', function() {
-            alert('탈퇴되었습니다.');
-            window.location.href = "/login/form"; // 로그인 페이지로 리디렉션
-        }).fail(function() {
-            alert('탈퇴 실패했습니다.');
-        });
-    }
-});
+        if (confirm('정말로 탈퇴 하시겠습니까?')) {
+            $.post('/delete/account', function() {
+                alert('탈퇴되었습니다.');
+                window.location.href = "/login/form"; // 로그인 페이지로 리디렉션
+            }).fail(function() {
+                alert('탈퇴 실패했습니다.');
+            });
+        }
+    });
